d00b35a338708161692a1e441b6660d51edaa408
Harri Evans <hevans9000@gmail.com>
2023-08-02 10:47:39 +0100

    feat(discord): auth

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,11 @@ module.exports = {
           500: '#dfcf99',
           800: '#a38d6d',
         },
+        discord: {
+          300: '#a2a6f8',
+          400: '#7983f5',
+          500: '#5865F2',
+        },
       },
       fontFamily: {
         fontin: ['fontin', 'sans-serif'],

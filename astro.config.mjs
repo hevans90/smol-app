48b0f60eaadd7bcc533873202499fc63c73ceb2f
Harri Evans <hevans9000@gmail.com>
2023-08-02 13:02:41 +0100

    fix: local netlify dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  })],
-  output: "server",
-  adapter: netlify()
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  server: { port: 3001 },
+  output: 'server',
+  adapter: netlify(),
 });

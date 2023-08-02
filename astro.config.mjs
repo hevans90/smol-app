e66afd32e1a164dfe66ef2a9b68f120ad2cea9f3
Harri Evans <hevans9000@gmail.com>
2023-08-02 15:45:56 +0100

    feat: add react; joke loading as example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import netlify from '@astrojs/netlify/functions';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  server: { port: 3001 },
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()],
+  server: {
+    port: 3001
+  },
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify()
 });
---
+++ src/components/react/Spinner.tsx
@@ -0,0 +1,5 @@
+import spinner from '../../icons/spinner.svg';
+
+export const Spinner = (props: { width?: number; height?: number }) => {
+  return <img src={spinner} {...props} />;
+};

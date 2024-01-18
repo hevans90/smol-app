b992a392778e5ba86d6952158f6c8f60185fdc86
Harri Evans <hevans9000@gmail.com>
2024-01-18 15:52:33 +0000

    feat(hasura): export seeds for enum tables

---
+++ hasura/seeds/default/1705593048567_item_order_type.sql
@@ -0,0 +1,4 @@
+SET check_function_bodies = false;
+INSERT INTO public.item_order_type (value) VALUES ('unique');
+INSERT INTO public.item_order_type (value) VALUES ('other');
+INSERT INTO public.item_order_type (value) VALUES ('transfiguredgem');

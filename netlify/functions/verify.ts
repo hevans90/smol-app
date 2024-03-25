3436da25fe844ceb2b97b2eaa184c371476b1729
Harri Evans <hevans9000@gmail.com>
2024-03-25 00:00:07 +0000

    fix(auth): scope to netlify function

--- netlify/functions/verify.ts
+++ netlify/functions/verify.ts
@@ -54,7 +54,7 @@ export const handler: Handler = async (event: HandlerEvent) => {
     grant_type: 'authorization_code',
     code: auth_code,
     redirect_uri: 'https://smol-app.netlify.app/api/auth',
-    scope: 'account:profile',
+    scope: 'account:profile account:leagues',
     code_verifier: poe_verifier,
   };
 

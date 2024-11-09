d4ec11c22bb44819402b408601f46fc47a7e6734
Harri Evans <hevans9000@gmail.com>
2024-11-09 23:32:11 +0000

    style(ladder): fancy fixed positioning & scrolling

--- src/components/react/graphql-provided/LeagueLadder.tsx
+++ src/components/react/graphql-provided/LeagueLadder.tsx
@@ -21,7 +21,7 @@ export const LeagueLadder = () => {
   const leagueName = league?.id;
 
   return (
-    <>
+    <div className="flex h-full w-full flex-col overflow-hidden">
       <div className="my-4 flex items-baseline">
         {league && league?.url ? (
           <>
@@ -39,7 +39,7 @@ export const LeagueLadder = () => {
       </div>
 
       <CharacterTable characters={charactersResponse?.character ?? []} />
-    </>
+    </div>
   );
 };
 
@@ -155,74 +155,78 @@ const CharacterTable: React.FC<{ characters: Character[] }> = ({
   };
 
   return (
-    <div className="mb-8 w-full overflow-x-auto">
-      <table className="min-w-full table-fixed text-left text-sm text-gray-400">
-        <thead className="bg-gray-900 bg-opacity-60 text-primary-500">
-          <tr>
-            <th className="w-16 px-4 py-2 font-medium">Rank</th>
-            <th className="w-32 px-4 py-2 font-medium">Account</th>
-            <th className="w-48 px-4 py-2 font-medium">Character</th>
-            <th
-              className="text w-32 cursor-pointer select-none px-4 py-3 font-medium"
-              onClick={() => handleSort('class')}
-            >
-              <button className="flex w-full whitespace-nowrap bg-transparent hover:bg-transparent">
-                <span>Class &nbsp;&nbsp;&nbsp;</span>
+    <div className="flex h-[90%] w-full flex-col">
+      {/* Table Wrapper */}
+      <div className="grow overflow-auto">
+        <table className="min-w-full table-fixed text-left text-sm text-gray-400">
+          <thead className="sticky top-0 bg-gray-900 text-primary-500">
+            <tr>
+              <th className="w-16 px-4 py-2 font-medium">Rank</th>
+              <th className="w-32 px-4 py-2 font-medium">Account</th>
+              <th className="w-48 px-4 py-2 font-medium">Character</th>
+              <th
+                className="text w-32 cursor-pointer select-none px-4 py-3 font-medium"
+                onClick={() => handleSort('class')}
+              >
+                <button className="flex w-full whitespace-nowrap bg-transparent hover:bg-transparent">
+                  <span>Class &nbsp;&nbsp;&nbsp;</span>
 
-                {sortConfig.key === null && (
-                  <span className="text-primary-800 opacity-50">↑ ↓</span>
-                )}
-
-                {sortConfig.key === 'class' ? (
-                  sortConfig.direction === 'asc' ? (
-                    <span>↑</span>
-                  ) : (
-                    <span>↓</span>
-                  )
-                ) : (
-                  ''
-                )}
-              </button>
-            </th>
-            <th className="w-32 px-4 py-2 font-medium">Level</th>
-            <th className="w-32 px-4 py-2 font-medium">Experience</th>
-          </tr>
-        </thead>
-        <tbody className="bg-gray-950 text-primary-800">
-          {currentCharacters.map((character) => (
-            <tr
-              key={character.id}
-              className="border-b border-primary-900 border-opacity-30 text-xs hover:bg-gray-900 hover:bg-opacity-50"
-            >
-              <td className="px-4 py-2">{character.rank}</td>
-              <td className="px-4 py-2">{character.poe_account_name}</td>
-              <td className="px-4 py-2">
-                <div>
-                  <a
-                    href={`https://www.pathofexile.com/account/view-profile/${character.poe_account_name}/characters?characterName=${character.name}`}
-                    target="_blank"
-                  >
-                    {character.name}
-                  </a>
-                  {character.retired && (
-                    <>
-                      <span className="text-primary-900"> (</span>
-                      <span className="font-bold text-red-700">Retired</span>
-                      <span className="text-primary-900">)</span>
-                    </>
+                  {sortConfig.key === null && (
+                    <span className="text-primary-800 opacity-50">↑ ↓</span>
                   )}
-                </div>
-              </td>
-              <td className="px-4 py-2">{character.class}</td>
-              <td className="px-4 py-2">{character.level}</td>
-              <td className="px-4 py-2">{character.experience}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
 
-      {/* Pagination Controls */}
-      <div className="mt-4 flex items-center justify-between text-primary-600">
+                  {sortConfig.key === 'class' ? (
+                    sortConfig.direction === 'asc' ? (
+                      <span>↑</span>
+                    ) : (
+                      <span>↓</span>
+                    )
+                  ) : (
+                    ''
+                  )}
+                </button>
+              </th>
+              <th className="w-32 px-4 py-2 font-medium">Level</th>
+              <th className="w-32 px-4 py-2 font-medium">Experience</th>
+            </tr>
+          </thead>
+          <tbody className="bg-gray-950 text-primary-800">
+            {currentCharacters.map((character) => (
+              <tr
+                key={character.id}
+                className="border-b border-primary-900 border-opacity-30 text-xs hover:bg-gray-900 hover:bg-opacity-50"
+              >
+                <td className="px-4 py-2">{character.rank}</td>
+                <td className="px-4 py-2">{character.poe_account_name}</td>
+                <td className="px-4 py-2">
+                  <div>
+                    <a
+                      href={`https://www.pathofexile.com/account/view-profile/${character.poe_account_name}/characters?characterName=${character.name}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {character.name}
+                    </a>
+                    {character.retired && (
+                      <>
+                        <span className="text-primary-900"> (</span>
+                        <span className="font-bold text-red-700">Retired</span>
+                        <span className="text-primary-900">)</span>
+                      </>
+                    )}
+                  </div>
+                </td>
+                <td className="px-4 py-2">{character.class}</td>
+                <td className="px-4 py-2">{character.level}</td>
+                <td className="px-4 py-2">{character.experience}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Pagination Controls - Always at the Bottom */}
+      <div className="mt-4 flex grow-0 items-center justify-between px-4 py-4 text-primary-600">
         <div>
           <label>Rows per page:</label>
           <select

2de80e3a00f75235c95c9b5085dcac39929e66a8
Harri Evans <hevans9000@gmail.com>
2025-01-07 12:08:24 +0000

    fix: item order webhook

--- netlify/functions/discord-notify-user.ts
+++ netlify/functions/discord-notify-user.ts
@@ -11,25 +11,22 @@ export const handler: Handler = async (event: HandlerEvent) => {
   invariant(event.body);
 
   const { discordUserId, message } = JSON.parse(
-    event?.body ?? ''
+    event?.body ?? '',
   ) as QueryParams;
 
   const messageToSend = `<@${discordUserId}> ${message}`;
 
   try {
-    await fetch(
-      'https://discord.com/api/webhooks/1180566422401855499/bvyyuFY6t-FH6TkZGIAn90MGK-p5Ix4tH3TLOlBKb5qVn4tfB8okP0JjoV6IN7876FEB',
-      {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          content: messageToSend,
-          username: 'smol-bot',
-        }),
-      }
-    );
+    await fetch(process.env.SMOL_DISCORD_ITEM_ORDER_WEBHOOK as string, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: messageToSend,
+        username: 'smol-bot',
+      }),
+    });
   } catch (e) {
     console.error(e);
   }
